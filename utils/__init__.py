c8c4c8e21b5c87014cc43d2151efda29a1f6d0e3
anonimity69 <shrayanmandal21@gmail.com>
2025-06-01 18:52:31 +0530

    inti commit author name

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,9 @@
+# ###################################### #
+# File: __init__.py                      #
+# Author: Shrayanendra Nath Mandal       #
+# Date: 06/01/2025                       #
+# Description: utils folder init         #
+# ###################################### #
 from .speechtotext import SpeechToText
 from .texttospeech import TextToSpeech
 from .alfredai_engine import AlfredChatbot
