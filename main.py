de699a2130a97f26cbcc95c1777c3e344611b16c
anonimity69 <shrayanmandal21@gmail.com>
2025-06-04 15:40:03 +0530

    to do later

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import tkinter as tk
-from gui.app import SimpleAlfredApp
+# import tkinter as tk
 
-def main():
-    root = tk.Tk()
-    app = SimpleAlfredApp(root)
-    root.mainloop()
+# def main():
+#     root = tk.Tk()
+#     app = SimpleAlfredApp(root)
+#     root.mainloop()
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
